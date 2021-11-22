04d0d9d956a72f68830ac8e6609c12e4035223eb
Crowdbotics <team@crowdbotics.com>
2021-11-22 02:37:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "still_timer_32077",
+  url: "https://still_timer_32077.botics.co",
+  api: "https://still_timer_32077.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
